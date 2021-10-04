8ded779706bf5cbe07a7be0c9f970eb08373ac6b
Romain Deneau <rdeneau@yahoo.fr>
2021-10-04 16:24:16 +0200

    improve hilowgame
    - change Char active pattern to ignore case
    - add Quit active pattern to have same level of abstraction in the whole match expression
    - inline buildRules to simplify the code
    - change naming of inner active patterns to avoid shadowing and improve readability

--- hilowgame/After.fs
+++ hilowgame/After.fs
@@ -2,39 +2,42 @@ module hilowgame.After
 
 open System
 
-let (|Char|_|) value =
-  let ok, c = Char.TryParse(value)
-  if ok then Some c else None
+let (===) expected actual =
+    (Char.ToUpper expected) = (Char.ToUpper actual)
 
-let (|High|_|) (one, two) = function
-  | Char 'h' | Char 'H' when one <= two -> Some () | _ -> None
+let (|Char|_|) (expected: char) value =
+  let parsed, actual = Char.TryParse(value)
+  if parsed && expected === actual then Some () else None
 
-let (|Low|_|) (one, two) = function
-  | Char 'l' | Char 'L' when two <= one -> Some () | _ -> None
+let (|High|_|) (currentCard, nextCard) = function
+  | Char 'h' when nextCard >= currentCard -> Some () | _ -> None
 
-let buildRules one two =
-  let pair = (one, two)
-  ((|High|_|) pair, (|Low|_|) pair)
+let (|Low|_|) (currentCard, nextCard) = function
+  | Char 'l' when nextCard <= currentCard -> Some () | _ -> None
+
+let (|Quit|_|) = function
+  | Char 'q' -> Some () | _ -> None
 
 let playGame (io : IO) =
 
-  let rec loop streak thisCard =
+  let rec loop streak currentCard =
     io.writef "Your current streak is %i cards.\n" streak
-    io.writef "Current card is: %i.\n" thisCard
+    io.writef "Current card is: %i.\n" currentCard
 
     let nextCard = io.drawCard ()
 #if INTERACTIVE
     io.writef "\n(DEBUG) Next Card: %i\n\n" nextCard
 #endif
-    let (|High|_|), (|Low|_|) = buildRules thisCard nextCard
+    let (|HighOk|_|) = (|High|_|) (currentCard, nextCard)
+    let (|LowOk|_|) = (|Low|_|) (currentCard, nextCard)
 
     io.writef "Do you go [h]igher or [l]ower (or [q]uit)? "
     match io.scanfn () with
-    | High | Low ->
+    | HighOk | LowOk ->
         io.writef "\nA lucky guess! Keep going...\n\n"
         loop (streak + 1) nextCard
 
-    | Char 'q' | Char 'Q' ->
+    | Quit ->
         io.writef "\nGoodbye! (for now... you'll be back)\n\n"
 
     | _ ->
